9612597f09d293a31eed7577fee956c5cab76713
Derya Dok <derya.dok@ogr.deu.edu.tr>
2019-05-24 17:03:06 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+﻿var counter = 0;
+function CatchMe() {
+    var gif = document.getElementById("catchMeGif");
+
+    var x = Math.floor(Math.random() * window.innerWidth);
+    var y = Math.floor(Math.random() * window.innerHeight);
+    
+    gif.style.left = x + "px";
+    gif.style.top = y + "px";
+
+    counter++;
+}
+
+function Caught() {
+    alert(counter + ". seferde yakalandım");
+}
